a19b350f0ebb460b4ed4d7fa09eeb510eec24f59
Tosha <zarotustra1982>
2019-07-02 23:29:03 +0300

    hometask_2

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -1,4 +1,55 @@
 package ru.skillbranch.devintensive.models
 
-class User {
+import java.util.*
+
+class User(
+    val id: String,
+    var firstName: String?,
+    var lastName: String?,
+    var avatar: String?,
+    var rating: Int = 0,
+    var respect: Int = 0,
+    val lastVisit: Date? = null,
+    val isOnline: Boolean = false
+) {
+    constructor(id: String, firstName: String?, lastName: String?) : this(
+        id = id,
+        firstName = firstName,
+        lastName = lastName,
+        avatar = null
+    )
+
+    constructor(id: String) : this(id, "John", "Doe $id")
+
+    init {
+        println("It's Alive!!!\n" + "And his name $firstName $lastName\n")
+    }
+
+    fun printMe(): Unit {
+        println(
+            """
+            id: $id
+            firstName: $firstName
+            lastName: $lastName
+            avatar: $avatar
+            rating: $rating
+            respect: $respect
+            lastVisit: $lastVisit
+            isOnline: $isOnline
+        """.trimIndent()
+        )
+    }
+
+    companion object Factory {
+        private var lastId: Int = -1
+        fun makeUser(fulName: String?): User {
+            lastId++
+            val parts: List<String>? = fulName?.split(" ")
+            val firstName = parts?.getOrNull(0)
+            val lastName = parts?.getOrNull(1)
+
+            return User(id = "$lastId", firstName = firstName, lastName = lastName)
+        }
+    }
+
 }
